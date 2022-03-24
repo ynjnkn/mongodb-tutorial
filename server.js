e5c8628ee2d5564525ea612f0e7532793f2fee1a
ynjnkn <yjun_k@hotmail.com>
2022-03-24 19:24:30 +0900

    4. Node.js로 MongoDB 다루기_Mongoose Connection 완료

--- server.js
+++ server.js
@@ -1,34 +1,50 @@
 // Dependencies
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 
 // Middlewares
 app.use(express.json());
 
 // DB
-const users = [{
-    user1: {
-        name: "Selina Kyle",
-        age: "31",
+const MONGODB_URI = "mongodb+srv://admin:dAX3uRtkdhUdqx4o@mongodb-tutorial.d7woi.mongodb.net/BlogService?retryWrites=true&w=majority";
+
+const server = async () => {
+    try {
+        await mongoose.connect(MONGODB_URI);
+        console.log("MongoDB Connected");
+
+        const users = [{
+            user1: {
+                name: "Selina Kyle",
+                age: "31",
+            }
+        }]
+
+        app.get("/users", function (req, res) {
+            return res.json({
+                users,
+            });
+        })
+
+        app.post("/users", function (req, res) {
+            users.push({
+                name: req.body.name,
+                age: req.body.age,
+            })
+            return res.send({
+                success: true,
+            })
+        })
+
+        app.listen(3000, function () {
+            console.log("Server listening on port 3000");
+        });
     }
-}]
+    catch (error) {
+        console.log({ error });
+    }
+}
 
-app.get("/users", function (req, res) {
-    return res.json({
-        users,
-    });
-})
-
-app.post("/users", function (req, res) {
-    users.push({
-        name: req.body.name,
-        age: req.body.age,
-    })
-    return res.send({
-        success: true,
-    })
-})
-
-app.listen(3000, function () {
-    console.log("Server listening on port 3000");
-});
+server();
