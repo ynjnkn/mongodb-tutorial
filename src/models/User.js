f24ef5d5d6213635db216611a11e1030a6710413
ynjnkn <yjun_k@hotmail.com>
2022-03-25 14:13:54 +0900

    4. Node.js로 MongoDB 다루기_Express에서 오류 처리하기 완료

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    nickname: {
+    username: {
         type: String,
         required: true,
     },
--- src/server.js
+++ src/server.js
@@ -4,49 +4,59 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-// Middlewares
-app.use(express.json());
-
 // DB
 const MONGODB_URI = "mongodb+srv://admin:dAX3uRtkdhUdqx4o@mongodb-tutorial.d7woi.mongodb.net/BlogService?retryWrites=true&w=majority";
-
-// Models
 const { User } = require('./models/User');
+
+// Middlewares
+app.use(express.json());
 
 const server = async () => {
     try {
         await mongoose.connect(MONGODB_URI);
         console.log("MongoDB Connected");
 
-        const users = [{
-            user1: {
-                name: "Selina Kyle",
-                age: "31",
-            }
-        }]
-
-        app.get("/users", function (req, res) {
+        app.get("/users", (req, res) => {
             return res.json({
-                users,
+
             });
         })
 
-        app.post("/users", function (req, res) {
-            users.push({
-                name: req.body.name,
-                age: req.body.age,
-            })
-            return res.send({
-                success: true,
-            })
+        app.post("/users", async (req, res) => {
+            try {
+                let { username, name } = req.body;
+                if (!username) {
+                    return res
+                        .status(400)
+                        .send({
+                            err: "Username is required.",
+                        });
+                }
+                if (!name || !name.first || !name.last) {
+                    return res
+                        .status(400)
+                        .send({
+                            err: "Both first and last name are required."
+                        });
+                }
+                const user = new User(req.body);
+                await user.save();
+                return res.send({
+                    user
+                })
+            }
+            catch (error) {
+                console.log(error)
+                return res
+                    .status(500)
+                    .send({ error: error.message });
+            }
         })
 
-        app.listen(3000, function () {
-            console.log("Server listening on port 3000");
-        });
+        app.listen(3000, () => console.log("Server listening on port 3000"));
     }
     catch (error) {
-        console.log({ error });
+        console.log({ error: error.message });
     }
 }
 
