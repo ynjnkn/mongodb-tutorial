913f352abd93082e8aece00992ba5754301b6c16
ynjnkn <yjun_k@hotmail.com>
2022-03-25 14:26:47 +0900

    4. Node.js로 MongoDB 다루기_GET/user 완료

--- src/server.js
+++ src/server.js
@@ -16,10 +16,19 @@ const server = async () => {
         await mongoose.connect(MONGODB_URI);
         console.log("MongoDB Connected");
 
-        app.get("/users", (req, res) => {
-            return res.json({
-
-            });
+        app.get("/users", async (req, res) => {
+            try {
+                const users = await User.find({});
+                return res
+                    .status(200)
+                    .json({ users });
+            }
+            catch (err) {
+                console.log(err)
+                return res
+                    .status(500)
+                    .send({ error: err.message });
+            }
         })
 
         app.post("/users", async (req, res) => {
@@ -28,35 +37,31 @@ const server = async () => {
                 if (!username) {
                     return res
                         .status(400)
-                        .send({
-                            err: "Username is required.",
-                        });
+                        .send({ err: "Username is required." });
                 }
                 if (!name || !name.first || !name.last) {
                     return res
                         .status(400)
-                        .send({
-                            err: "Both first and last name are required."
-                        });
+                        .send({ err: "Both first and last name are required." });
                 }
                 const user = new User(req.body);
                 await user.save();
-                return res.send({
-                    user
-                })
+                return res
+                    .status(201)
+                    .send({ user })
             }
-            catch (error) {
-                console.log(error)
+            catch (err) {
+                console.log(err)
                 return res
                     .status(500)
-                    .send({ error: error.message });
+                    .send({ error: err.message });
             }
         })
 
         app.listen(3000, () => console.log("Server listening on port 3000"));
     }
-    catch (error) {
-        console.log({ error: error.message });
+    catch (err) {
+        console.log({ error: err.message });
     }
 }
 
