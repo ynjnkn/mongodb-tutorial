2fa96dc1cb3495eb4c676fbf76a263f93edb5964
ynjnkn <yjun_k@hotmail.com>
2022-03-26 16:20:52 +0900

    5. 관계된 데이터 관리하기_create Blog document 완료

--- src/routes/blogRouter.js
+++ src/routes/blogRouter.js
@@ -1,13 +1,37 @@
 const { Router } = require("express");
 const blogRouter = Router();
-const mongoose = require("mongoose");
+const { isValidObjectId } = require("mongoose");
 const { Blog } = require("../models/Blog");
+const { User } = require("../models/User");
 
 blogRouter.post("/", async (req, res) => {
     try {
+        console.log("req.body", req.body);
+        const { title, content, isLive, userId } = req.body;
+        if (!title)
+            return res.status(400).send({ error: "Title is required." });
+        if (typeof (title) !== "string")
+            return res.status(400).send({ error: "Title must be a string." });
+        if (!content)
+            return res.status(400).send({ error: "Content is required." });
+        if (typeof (content) !== "string")
+            return res.status(400).send({ error: "Content must be a string." });
+        if (isLive && isLive !== "boolean")
+            return res.status(400).send({ error: "isLive must be a boolean." });
+        if (!isValidObjectId(userId))
+            return res.status(400).send({ error: "userId is invalid." });
+        let user = await User.findById(userId);
+        if (!user)
+            return res.status(400).send({ error: "User is not found." });
+
+        let blog = new Blog({
+            ...req.body, user
+        });
+        await blog.save();
+
         return res
             .status(200)
-            .send();
+            .send({ blog });
     }
     catch (err) {
         console.log({ error: { name: err.name, message: err.message } })
