0b476ae36b46ace0dba0dc07baf8683c04affa5c
ynjnkn <yjun_k@hotmail.com>
2022-03-26 17:31:18 +0900

    5. 관계된 데이터 관리하기_Comment model 만들기 완료

---
+++ src/models/Comment.js
@@ -0,0 +1,21 @@
+const { Schema, model, Types: { ObjectId } } = require("mongoose");
+
+const commentSchema = new Schema({
+    content: {
+        type: String,
+        required: true,
+    },
+    user: {
+        type: ObjectId,
+        required: true,
+        ref: "user",
+    },
+    blog: {
+        type: ObjectId,
+        required: true,
+        ref: "blog",
+    }
+}, { timestamps: true });
+
+const Comment = model("comment", commentSchema);
+module.exports = { Comment };
