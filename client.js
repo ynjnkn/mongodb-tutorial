8f881f2f208cdb88062639c8fc6b7216d18972bd
ynjnkn <yjun_k@hotmail.com>
2022-03-28 12:21:57 +0900

    6. 관계된 데이터 효율적으로 읽기_Client 코드 리팩토링 & 마무리 완료

--- client.js
+++ client.js
@@ -1,16 +1,24 @@
 const axios = require("axios");
 
+const SERVER_URI = "http://localhost:3000";
+
 const test = async () => {
-    let { data: { blogs } } = await axios.get("http://localhost:3000/blogs");
-    console.log("변경 전", blogs[0]);
-    blogs = await Promise.all(blogs.map(async blog => {
-        const responseUser = await axios.get(`http://localhost:3000/users/${blog.user}`);
-        const responseComments = await axios.get(`http://localhost:3000/blogs/${blog._id}/comments`);
-        blog.user = responseUser.data.user;
-        blog.comments = responseComments.data.comments;
-        return blog;
-    }));
-    console.log("변경 후", blogs[0]);
+    let { data: { blogs } } = await axios.get(`${SERVER_URI}/blogs`);
+    blogs = await Promise.all(
+        blogs.map(async blog => {
+            const [responseUser, responseComments] = await Promise.all([
+                await axios.get(`${SERVER_URI}/users/${blog.user}`),
+                await axios.get(`${SERVER_URI}/blogs/${blog._id}/comments`)
+            ])
+            blog.user = responseUser.data.user;
+            blog.comments = await Promise.all(
+                responseComments.data.comments.map(async comment => {
+                    const { data: { user } } = await axios.get(`${SERVER_URI}/users/${comment.user}`);
+                    comment.user = user;
+                    return comment;
+                }));
+            return blog;
+        }));
 }
 
 test();
