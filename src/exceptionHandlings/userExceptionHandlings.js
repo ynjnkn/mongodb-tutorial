2034127f8f55560371021c5a88f1566686f1cf2a
ynjnkn <yjun_k@hotmail.com>
2022-04-05 18:19:50 +0900

    04. Node.js로 MongoDB 다루기_PUT/user/:userId_userRouter 예외처리 API 단위로 묶기

--- src/exceptionHandlings/userExceptionHandlings.js
+++ src/exceptionHandlings/userExceptionHandlings.js
@@ -1,63 +1,139 @@
 const mongoose = require("mongoose");
 const { User } = require("../models/User");
 
-const isInvalidUserId = async (userId) => {
-  if (!mongoose.isValidObjectId(userId)) {
-    return { error: "The provided User ID is invalid." };
-  }
-  const user = await User.findById(userId);
-  if (!user) {
-    return { error: "User is not found." };
-  }
-  return null;
-};
-
-const isNotFoundUser = (user) => {
-  if (!user) {
-    return { error: "User is not found." };
-  }
-  return null;
-};
-
-const isWrongUsername = async (username) => {
-  if (!username) return { error: "Username is required." };
+const isExceptionCreateUser = async (username, name, age, email, res) => {
+  // Check Username
+  if (!username)
+    return res.status(400).send({ error: "Username is required." });
   if (typeof username !== "string")
-    return { error: "Username must be a string." };
+    return res.status(400).send({ error: "Username must be a string." });
   const existingUsername = await User.findOne({ username });
   if (existingUsername)
-    return { error: "The provided username already exists." };
-  return null;
-};
-
-const isWrongName = (name) => {
+    return res
+      .status(400)
+      .send({ error: "The provided username already exists." });
+  // Check Name
   if (!name || !name.first || !name.last) {
-    return { error: "Both first and last name are required." };
+    return res
+      .status(400)
+      .send({ error: "Both first and last name are required." });
   }
   if (typeof name.first !== "string" || typeof name.last !== "string") {
-    return { error: "Name must be a string." };
+    return res.status(400).send({ error: "Name must be a string." });
+  }
+  // Check Age
+  if (!age) return res.status(400).send({ error: "Age is required." });
+  if (typeof age !== "number")
+    return res.status(400).send({ error: "Age must be a number." });
+  // Check Email
+  if (!email) return res.status(400).send({ error: "Email is required." });
+  if (typeof email !== "string")
+    return res.status(400).send({ error: "email must be a string." });
+  const existingEmail = await User.findOne({ email });
+  if (existingEmail)
+    return res
+      .status(400)
+      .send({ error: "The provided email already exists." });
+  return null;
+};
+
+const isExceptionReadAUser = async (userId, res) => {
+  // Check ObjectId
+  if (!mongoose.isValidObjectId(userId)) {
+    return res.status(400).send({ error: "The provided ObjectId is invalid." });
+  }
+  // Check if a User with the ObjectId exists.
+  const user = await User.findById(userId);
+  if (!user) {
+    return res.status(400).send({ error: "User is not found." });
   }
   return null;
 };
 
-const isWrongAge = (age) => {
-  if (!age) return { error: "Age is required." };
-  if (typeof age !== "number") return { error: "Age must be a number." };
+const isExceptionPutAUser = async (userId, myNewUserInfo, res) => {
+  // Check ObjectId - Valid ObjectId? [v]
+  if (!mongoose.isValidObjectId(userId)) {
+    return res.status(400).send({ error: "The provided User ID is invalid." });
+  }
+  // Check ObjectId - Matched User? [v]
+  const myCurrentUserInfo = await User.findById(userId);
+  if (!myCurrentUserInfo) {
+    return res.status(400).send({ error: "User is not found." });
+  }
+  // Check Username - Duplicate Username? [v]
+  if (
+    myNewUserInfo.username &&
+    myNewUserInfo.username !== myCurrentUserInfo.username
+  ) {
+    const globalDuplicateUsername = await User.findOne({
+      username: myNewUserInfo.username,
+    });
+    if (globalDuplicateUsername) {
+      return res
+        .status(400)
+        .send({ error: "The provided username already exists." });
+    }
+  }
+  // Check Username - Type String? [v]
+  if (myNewUserInfo.username && typeof myNewUserInfo.username !== "string") {
+    return res.status(400).send({ error: "Username must be a string." });
+  }
+  // Check Name - Type String? [v]
+  if (
+    myNewUserInfo.name.first &&
+    myNewUserInfo.name.last &&
+    (typeof myNewUserInfo.name.first !== "string" ||
+      typeof myNewUserInfo.name.last !== "string")
+  ) {
+    return res.status(400).send({ error: "Name must be a string." });
+  }
+  // Check Name - Both First and Last Name Exist? [v]
+  if (
+    myNewUserInfo.name &&
+    (!myNewUserInfo.name.first || !myNewUserInfo.name.last)
+  ) {
+    return res
+      .status(400)
+      .send({ error: "Both first and last name are required." });
+  }
+  // Check Age - Type Number? [v]
+  if (myNewUserInfo.age && typeof myNewUserInfo.age !== "number") {
+    return res.status(400).send({ error: "Age must be a number." });
+  }
+  // Check Email - Duplicate Email? [v]
+  if (myNewUserInfo.email && myNewUserInfo.email !== myCurrentUserInfo.email) {
+    const globalDuplicateEmail = await User.findOne({
+      email: myNewUserInfo.email,
+    });
+    if (globalDuplicateEmail) {
+      return res
+        .status(400)
+        .send({ error: "The provided email already exists." });
+    }
+  }
+  // Check Email - Type String? [v]
+  if (myNewUserInfo.email && typeof myNewUserInfo.email !== "string") {
+    return res.status(400).send({ error: "Email must be a string." });
+  }
   return null;
 };
 
-const isWrongEmail = async (email) => {
-  if (!email) return { error: "Email is required." };
-  if (typeof email !== "string") return { error: "email must be a string." };
-  const existingEmail = await User.findOne({ email });
-  if (existingEmail) return { error: "The provided email already exists." };
+const isExceptionDeleteAUser = async (userId, res) => {
+  // Check ObjectId
+  if (!mongoose.isValidObjectId(userId)) {
+    return res.status(400).send({ error: "The provided ObjectId is invalid." });
+  }
+  // Check if a User with the ObjectId exists.
+  const user = await User.findById(userId);
+  if (!user) {
+    return res.status(400).send({ error: "User is not found." });
+  }
   return null;
 };
 
 module.exports = {
-  isInvalidUserId,
-  isNotFoundUser,
-  isWrongUsername,
-  isWrongName,
-  isWrongAge,
-  isWrongEmail,
+  isExceptionCreateUser,
+  isExceptionReadAUser,
+  isExceptionPutAUser,
+  isExceptionDeleteAUser,
 };
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,36 +1,23 @@
 // Dependencies
 const { Router } = require("express");
 const userRouter = Router();
-const mongoose = require("mongoose");
 
 // Models
 const { User } = require("../models/User");
 
 // Exception Handlings
 const {
-  isInvalidUserId,
-  isNotFoundUser,
-  isWrongUsername,
-  isWrongName,
-  isWrongAge,
-  isWrongEmail,
+  isExceptionCreateUser,
+  isExceptionReadAUser,
+  isExceptionPutAUser,
+  isExceptionDeleteAUser,
 } = require("../exceptionHandlings/userExceptionHandlings");
 
 userRouter.post("/", async (req, res) => {
   try {
     let { username, name, age, email } = req.body;
-
-    if (await isWrongUsername(username)) {
-      return res.status(400).send(await isWrongUsername(username));
-    }
-    if (isWrongName(name)) {
-      return res.status(400).send(isWrongName(name));
-    }
-    if (isWrongAge(age)) {
-      return res.status(400).send(isWrongAge(age));
-    }
-    if (await isWrongEmail(email)) {
-      return res.status(400).send(await isWrongEmail(email));
+    if (await isExceptionCreateUser(username, name, age, email, res)) {
+      return;
     }
     const user = new User(req.body);
     await user.save();
@@ -58,13 +45,10 @@ userRouter.get("/", async (req, res) => {
 userRouter.get("/:userId", async (req, res) => {
   try {
     const { userId } = req.params;
-    if (await isInvalidUserId(userId)) {
-      return res.status(400).send(await isInvalidUserId(userId));
+    if (await isExceptionReadAUser(userId, res)) {
+      return;
     }
     const user = await User.findById(userId);
-    if (isNotFoundUser(user)) {
-      return res.status(400).send(isNotFoundUser(user));
-    }
     return res.status(200).send({ user });
   } catch (err) {
     console.log({ error: { name: err.name, message: err.message } });
@@ -78,73 +62,12 @@ userRouter.put("/:userId", async (req, res) => {
   try {
     const { userId } = req.params;
     const myNewUserInfo = req.body;
-    const myCurrentUserInfo = await User.findById(userId);
-
-    // userId가 유효한지 확인
-    if (await isInvalidUserId(userId)) {
-      return res.status(400).send(await isInvalidUserId(userId));
+    if (await isExceptionPutAUser(userId, myNewUserInfo, res)) {
+      return;
     }
-    // username 중복 확인 (단, 자신의 기존 username은 괜찮음)
-    if (
-      myNewUserInfo.username &&
-      myNewUserInfo.username !== myCurrentUserInfo.username
-    ) {
-      const globalDuplicateUsername = await User.findOne({
-        username: myNewUserInfo.username,
-      });
-      if (globalDuplicateUsername) {
-        return res
-          .status(400)
-          .send({ error: "The provided username already exists." });
-      }
-    }
-    // email 중복 확인 (단, 자신의 기존 email은 괜찮음)
-    if (
-      myNewUserInfo.email &&
-      myNewUserInfo.email !== myCurrentUserInfo.email
-    ) {
-      const globalDuplicateEmail = await User.findOne({
-        email: myNewUserInfo.email,
-      });
-      if (globalDuplicateEmail) {
-        return res
-          .status(400)
-          .send({ error: "The provided email already exists." });
-      }
-    }
-    // 새로운 username이 있는 경우? string인지 확인
-    if (myNewUserInfo.username && typeof myNewUserInfo.username !== "string") {
-      return res.status(400).send({ error: "Username must be a string." });
-    }
-    // 새로운 name이 있는 경우? string인지 확인
-    if (
-      myNewUserInfo.name.first &&
-      myNewUserInfo.name.last &&
-      (typeof myNewUserInfo.name.first !== "string" ||
-        typeof myNewUserInfo.name.last !== "string")
-    ) {
-      return res.status(400).send({ error: "Name must be a string." });
-    }
-    // 새로운 age가 있는 경우? number인지 확인
-    if (myNewUserInfo.age && typeof myNewUserInfo.age !== "number") {
-      return res.status(400).send({ error: "Age must be a number." });
-    }
-    // 새로운 email이 있는 경우? string인지 확인
-    if (myNewUserInfo.email && typeof myNewUserInfo.email !== "string") {
-      return res.status(400).send({ error: "Email must be a string." });
-    }
-    // 새로운 name이 있는 경우? first, last 둘 다 있는지 확인
-    if (
-      myNewUserInfo.name &&
-      (!myNewUserInfo.name.first || !myNewUserInfo.name.last)
-    ) {
-      return res
-        .status(400)
-        .send({ error: "Both first and last name are required." });
-    }
-
-    const user = await User.findByIdAndUpdate(userId, req.body, { new: true });
-
+    const user = await User.findByIdAndUpdate(userId, myNewUserInfo, {
+      new: true,
+    });
     return res.status(200).send({ user });
   } catch (err) {
     console.log({ error: { name: err.name, message: err.message } });
@@ -157,13 +80,10 @@ userRouter.put("/:userId", async (req, res) => {
 userRouter.delete("/:userId", async (req, res) => {
   try {
     const { userId } = req.params;
-    if (await isInvalidUserId(userId)) {
-      return res.status(400).send({ error: "User is not found." });
+    if (await isExceptionDeleteAUser(userId, res)) {
+      return;
     }
     const user = await User.findByIdAndDelete(userId);
-    if (isNotFoundUser(user)) {
-      return res.status(400).send({ error: "User is not found." });
-    }
     return res.status(200).send({ user });
   } catch (err) {
     console.log({ error: { name: err.name, message: err.message } });
