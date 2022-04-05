19fbdd4f3c1f21876a573a484a13137c0b7c67f9
ynjnkn <yjun_k@hotmail.com>
2022-04-05 19:18:40 +0900

    05. 관계된 데이터 관리하기_User와 관계된 Blog Model [완료]

---
+++ src/models/Blog.js
@@ -0,0 +1,14 @@
+const { Schema, model, Types } = require("mongoose");
+
+const blogSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    isLive: { type: Boolean, required: true, default: false },
+    user: { type: Types.ObjectId, required: true, ref: "user" },
+  },
+  { timestamps: true }
+);
+
+const Blog = model("blog", blogSchema);
+module.exports = { Blog };
